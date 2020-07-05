94680a4dcfca13b8bf8278c17514a6af4e429fd8
Ganeesya <ganephone@gmail.com>
2020-07-05 23:30:06 +0900

    とりあえず固有コードを登録

---
+++ Assets/clickMove.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class clickMove : MonoBehaviour
+{
+    private Boolean isDrag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            var newPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+//            newPos.z = 0;
+//            transform.position = newPos;
+//        }
+    }
+
+    private void OnMouseDrag()
+    {
+        var newPos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        newPos.z = 0;
+        transform.position = newPos;
+    }
+}
