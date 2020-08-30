87c1138e60ca131eee2c83bf682244a5649a394f
Ganeesya <ganephone@gmail.com>
2020-08-30 23:04:38 +0900

    合成結果の出力機構の作成

--- Assets/Live2DLivePaint/PaintManager.cs
+++ Assets/Live2DLivePaint/PaintManager.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Live2D.Cubism.Core;
+using Live2D.Cubism.Rendering;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 /**
  * 描画コントロールに必要なヒエラルキー上のオブジェクトを入れてPaintControllerで参照するためのCompo
@@ -14,6 +16,8 @@ public class PaintManager : MonoBehaviour
     public Camera paintingCamera;
 
     public ColorPickerTriangle colorPicker;
+
+    public CubismModel saveTarget;
 
     [NonSerialized]
     public Mesh penMesh;
@@ -29,6 +33,49 @@ public class PaintManager : MonoBehaviour
         RenderTexture.active = paintingCamera.targetTexture;
         GL.Clear(true, true, Color.clear );
         RenderTexture.active = current;
+    }
+
+    public void OutputPaintedTexture()
+    {
+        String path;
+#if UNITY_EDITOR
+        path = Directory.GetCurrentDirectory();//Editor上では普通にカレントディレクトリを確認
+#else
+        FilePath = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');//EXEを実行したカレントディレクトリ (ショートカット等でカレントディレクトリが変わるのでこの方式で)
+#endif
+        path = path + "\\OutputTest\\";
+        Directory.CreateDirectory(path);
+        Debug.Log(path);
+        OutputPaintedTexture(path);
+    }
+
+    private void OutputPaintedTexture( String outPath )
+    {
+        var sampleMaterials = saveTarget.Drawables.Select( it => it.GetComponent<CubismRenderer>() );
+        var samplePaintedCubismRenderer = sampleMaterials.Where(it => Regex.IsMatch(it.Material.name, "Rp\\s*\\(Instance\\)$")).First();
+        var mainTexture = samplePaintedCubismRenderer.Material.mainTexture;
+        
+        RenderTexture drawingBuffer = new RenderTexture( mainTexture.width, mainTexture.height, 1 );
+        
+        Material bufferCreater = new Material(samplePaintedCubismRenderer.Material);
+        bufferCreater.SetInt("isPremultiedAlphaOut", 0);
+        
+        Graphics.Blit(mainTexture, drawingBuffer, bufferCreater);
+        
+        var currentActive = RenderTexture.active;
+        
+        Texture2D writeOut = new Texture2D( mainTexture.width, mainTexture.height, TextureFormat.RGBA32, false );
+        RenderTexture.active = drawingBuffer;
+        writeOut.ReadPixels(new Rect(0f,0f, mainTexture.width, mainTexture.height),0,0 );
+        writeOut.Apply();
+
+        RenderTexture.active = currentActive;
+
+        var pngData = ImageConversion.EncodeToPNG(writeOut);
+        File.WriteAllBytes(outPath + mainTexture.name + ".png", pngData );
+        
+        Destroy( writeOut );
+        Destroy( drawingBuffer );
     }
 
     // Update is called once per frame
